5193e3b94b7d7de513b14c55e298da3f2e8b8a30
SHADOW2\Riker <Shadowninja0409@att.net>
2018-02-26 18:26:19 -0500

    Shader program implemented

--- src/enginetester/MainGameLoop.java
+++ src/enginetester/MainGameLoop.java
@@ -6,6 +6,7 @@ import renderengine.DisplayManager;
 import renderengine.Loader;
 import renderengine.RawModel;
 import renderengine.Renderer;
+import shaders.StaticShader;
 
 public class MainGameLoop {
 
@@ -14,6 +15,7 @@ public class MainGameLoop {
 		
 		Loader loader = new Loader();
 		Renderer renderer = new Renderer();
+		StaticShader shader = new StaticShader();
 		
 		float[] verticies = {
 				-0.5f, 0.5f, 0,
@@ -32,13 +34,15 @@ public class MainGameLoop {
 		
 		while(!Display.isCloseRequested()){
 			renderer.prepare();
-
+			shader.start();
 			//RawModel
-			
 			renderer.render(model);
+			shader.stop();
 			DisplayManager.updateDisplay();
 		}
 
+		
+		shader.cleanUp();
 		loader.cleanUp();
 		DisplayManager.closeDisplay();
 	}
