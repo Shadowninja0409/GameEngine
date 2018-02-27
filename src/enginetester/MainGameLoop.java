aa9f0294b0bac9ffe16880a0670a232421f15640
SHADOW2\Riker <Shadowninja0409@att.net>
2018-02-26 23:01:27 -0500

    image rendering, shader changes, and textured models implemented

--- src/enginetester/MainGameLoop.java
+++ src/enginetester/MainGameLoop.java
@@ -2,11 +2,13 @@ package enginetester;
 
 import org.lwjgl.opengl.Display;
 
+import models.RawModel;
+import models.TexturedModel;
 import renderengine.DisplayManager;
 import renderengine.Loader;
-import renderengine.RawModel;
 import renderengine.Renderer;
 import shaders.StaticShader;
+import textures.ModelTexture;
 
 public class MainGameLoop {
 
@@ -18,25 +20,33 @@ public class MainGameLoop {
 		StaticShader shader = new StaticShader();
 		
 		float[] verticies = {
-				-0.5f, 0.5f, 0,
-				-0.5f, -0.5f, 0,
-				
-				0.5f, -0.5f, 0,
-				0.5f, 0.5f, 0,
+				-0.5f, 0.5f, 0,  //V0
+				-0.5f, -0.5f, 0, //V1
+				0.5f, -0.5f, 0,  //V2
+				0.5f, 0.5f, 0,   //V3
 		};
 		
 		int[] indices = {
-				0,1,3,
-				3,1,2
+				0,1,3, // top left triangle
+				3,1,2  // bottom right triangle
 		};
 		
-		RawModel model = loader.loadToVAO(verticies, indices);
+		float[] textureCoords = {
+				0,0, //V0
+				0,1, //V1
+				1,1, //V2
+				1,0  //V3
+		};
+		
+		RawModel model = loader.loadToVAO(verticies, textureCoords, indices);
+		ModelTexture texture = new ModelTexture(loader.loadTexture("3004391"));
+		TexturedModel texturedModel = new TexturedModel(model, texture); 
 		
 		while(!Display.isCloseRequested()){
 			renderer.prepare();
 			shader.start();
 			//RawModel
-			renderer.render(model);
+			renderer.render(texturedModel);
 			shader.stop();
 			DisplayManager.updateDisplay();
 		}
